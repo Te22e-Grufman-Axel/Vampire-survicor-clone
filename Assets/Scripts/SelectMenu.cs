e38fa4532053d71814d3b43f4fa94c53c1dd0e81
Axel grufman <axel@grufman.se>
2025-09-17 15:33:52 +0200

    desing mode done

---
+++ Assets/Scripts/SelectMenu.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SelectMenu : MonoBehaviour
+{
+    [Header("Top Level Panels")]
+    [SerializeField] private GameObject valueModePanel;
+    [SerializeField] private GameObject designModePanel;
+    [SerializeField] private Button toValueModeButton;
+    [SerializeField] private Button toDesignModeButton;
+
+    [Header("Design Mode Subpanels")]
+    [SerializeField] private GameObject pngPanel;
+    [SerializeField] private GameObject shapeColourPanel;
+    [SerializeField] private Toggle pngToggle; 
+
+    [Header("Shape & Colour Subpanels")]
+    [SerializeField] private GameObject colourPickerPanel;
+    [SerializeField] private GameObject shapePickerPanel;
+    [SerializeField] private Button colourPickerButton;
+    [SerializeField] private Button shapePickerButton;
+
+    private void Start()
+    {
+        valueModePanel.SetActive(true);
+        designModePanel.SetActive(false);
+
+        pngToggle.onValueChanged.AddListener(OnPngToggleChanged);
+
+        colourPickerButton.onClick.AddListener(() => ShowShapeColourSubpanel(true));
+        shapePickerButton.onClick.AddListener(() => ShowShapeColourSubpanel(false));
+        toValueModeButton.onClick.AddListener(GoToValueMode);
+        toDesignModeButton.onClick.AddListener(GoToDesignMode);
+
+        pngToggle.isOn = true;
+        ShowDesignSubpanel(pngToggle.isOn);
+    }
+
+    public void GoToDesignMode()
+    {
+        valueModePanel.SetActive(false);
+        designModePanel.SetActive(true);
+        pngToggle.isOn = true;
+        ShowDesignSubpanel(true);
+    }
+
+    public void GoToValueMode()
+    {
+        valueModePanel.SetActive(true);
+        designModePanel.SetActive(false);
+    }
+
+    private void OnPngToggleChanged(bool isOn)
+    {
+        ShowDesignSubpanel(isOn);
+    }
+
+    private void ShowDesignSubpanel(bool showPng)
+    {
+        pngPanel.SetActive(showPng);
+        shapeColourPanel.SetActive(!showPng);
+    }
+
+    private void ShowShapeColourSubpanel(bool showColour)
+    {
+        colourPickerPanel.SetActive(showColour);
+        shapePickerPanel.SetActive(!showColour);
+    }
+}
--- Assets/Scripts/ShapePicker.cs
+++ Assets/Scripts/ShapePicker.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class ShapePicker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Button[] buttons;
+    private int selectedIndex = -1;
+
+    private Color selectedColor = Color.green;
+    private Color normalColor = Color.white;
+
     void Start()
     {
-        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int idx = i;
+            buttons[i].onClick.AddListener(() => OnButtonClicked(idx));
+            SetButtonColor(i, normalColor);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnButtonClicked(int idx)
     {
-        
+        if (selectedIndex == idx)
+        {
+            SetButtonColor(idx, normalColor);
+            selectedIndex = -1;
+        }
+        else
+        {
+            if (selectedIndex != -1)
+                SetButtonColor(selectedIndex, normalColor);
+
+
+            SetButtonColor(idx, selectedColor);
+            selectedIndex = idx;
+        }
+    }
+
+    private void SetButtonColor(int idx, Color color)
+    {
+        var colors = buttons[idx].colors;
+        colors.normalColor = color;
+        colors.selectedColor = color;
+        buttons[idx].colors = colors;
+    }
+
+    public int GetSelectedButtonId()
+    {
+        return selectedIndex == -1 ? 0 : selectedIndex + 1;
     }
 }
